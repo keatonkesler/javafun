524f6d74e7b37ac13d40155e164a82aba0ae1270
keatonkesler <keatonkesler@gmail.com>
2017-10-13 09:51:04 -0600

    second
    comments and stuff

--- git_tester/src/git_tester/git_test.java
+++ git_tester/src/git_tester/git_test.java
@@ -5,7 +5,7 @@ public class git_test {
 	public static void main(String[] args) {
 		System.out.println("A Git Test");
 		System.out.println("A Git Test");
-		System.out.println("A Git Test");
+		
 	}
 
 }
