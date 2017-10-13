73efb4209b583f6622493b08f13af746ebbca86c
keatonkesler <keatonkesler@gmail.com>
2017-10-13 09:52:08 -0600

    third
    mre comments

--- git_tester/src/git_tester/git_test.java
+++ git_tester/src/git_tester/git_test.java
@@ -5,7 +5,10 @@ public class git_test {
 	public static void main(String[] args) {
 		System.out.println("A Git Test");
 		System.out.println("A Git Test");
-		
+		System.out.println("A Git Test");
+		System.out.println("A Git Test");
+		System.out.println("A Git Test");
+		System.out.println("A Git Test");
 	}
 
 }
